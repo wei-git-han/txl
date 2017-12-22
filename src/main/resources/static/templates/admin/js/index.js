4eca3684d2e94d0497e3629bbd56648257ec0206
xiayujie <xiayujie@cd167626-96c9-be43-bac0-b091b8c5cdea>
2017-12-22 09:52:24 +0000

    ****更改颜色》》》》》》》》》》
    git-svn-id: https://172.16.201.6:444/svn/RJ_BGPT/GENERAL_APP/txl_app@11177 cd167626-96c9-be43-bac0-b091b8c5cdea

--- src/main/resources/static/templates/admin/js/index.js
+++ src/main/resources/static/templates/admin/js/index.js
@@ -93,9 +93,9 @@ var pageModule = function(){
                      }},
                      {display:"操作",name:"do",width:"13%",align:"center",paixu:false,render:function(rowdata){
                     	 if(rowdata.isSc == 0 ){
-                    		 return '<a class="sc" title="收藏" href="javascript:addscfn(\''+rowdata.userid+'\')"><i class="fa fa-star"></i></a>';
+                    		 return '<a class="ysc" title="收藏" href="javascript:addscfn(\''+rowdata.userid+'\')"><i class="fa fa-star"></i></a>';
                     	 }else{
-                    		 return '<a class="ysc" title="取消收藏" href="javascript:delscfn(\''+rowdata.userid+'\')"><i class="fa fa-star"></i></a>';
+                    		 return '<a class="sc" title="取消收藏" href="javascript:delscfn(\''+rowdata.userid+'\')"><i class="fa fa-star"></i></a>';
                     	 }
                       }}
                  ],
