60718a2a7f33c28aa5a303d65eb637568d9dd452
quxinyong <quxinyong@cd167626-96c9-be43-bac0-b091b8c5cdea>
2017-12-25 07:35:36 +0000

    搜索接口：调整搜索接口代码。
    git-svn-id: https://172.16.201.6:444/svn/RJ_BGPT/GENERAL_APP/txl_app@11332 cd167626-96c9-be43-bac0-b091b8c5cdea

--- src/main/java/com/css/webservice/controller/SearchApiController.java
+++ src/main/java/com/css/webservice/controller/SearchApiController.java
@@ -1,35 +1,30 @@
 package com.css.webservice.controller;
 
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.alibaba.fastjson.JSONObject;
 import com.css.addbase.JSONUtil;
 import com.css.addbase.PinYinUtil;
-import com.css.addbase.orgservice.OrgService;
-import com.css.base.entity.SSOUser;
+import com.css.base.utils.CurrentUser;
 import com.css.base.utils.Response;
-import com.css.base.utils.StringUtils;
 import com.css.txl.service.TxlUserService;
-import com.css.webservice.entity.SearchEntity;
 
 @Controller
 @RequestMapping("/api/search")
 public class SearchApiController {
 	@Autowired
 	private TxlUserService txlUserService;
-	@Autowired
-	private OrgService orgService;
 	@Value("${csse.txl.appId}")
 	private String appId;
 	/**
@@ -40,10 +35,10 @@ public class SearchApiController {
 	@ResponseBody
 	@RequestMapping("/data")
 	public void data(HttpServletRequest request) {
-		sysPring(request);
-		JSONObject obj=null;//jsonFailed(entity);
+		sysPrint(request);
+		JSONObject obj=jsonFailed(request);
 		if(obj==null){return;}
-		String userId=obj.getString("userId");
+		String userId=CurrentUser.getUserId();
 		String[] keyWords=obj.getObject("keyWords", String[].class);
 		Map<String,Object> map=new HashMap<String,Object>();
 		map.put("keywords", keyWords);
@@ -55,9 +50,9 @@ public class SearchApiController {
 		JSONObject result=JSONUtil.resultSuccess(total,appId, link);
 		Response.json(result);
 	}
-	private JSONObject jsonFailed(SearchEntity entity) {
-		String token = entity.getToken();
-		String[] keyWords=entity.getKeyWords();
+	private JSONObject jsonFailed(HttpServletRequest request) {
+		String kds=request.getParameter("keyWords");
+		String[] keyWords=toArray(kds);
 		
 		JSONObject jsonFailed = new JSONObject();
 		jsonFailed.put("result", "failed");
@@ -66,27 +61,54 @@ public class SearchApiController {
 			Response.json(jsonFailed);
 			return null;
 		}
-		SSOUser user = orgService.getSUser(token);
-		String userId = user != null ? user.getUserId() : "";
-		String userName=user!=null?user.getFullname():"";
-		if (user == null||StringUtils.isBlank(userId)) {
-			jsonFailed.put("msg", "token参数为空或不正确！");
-			Response.json(jsonFailed);
-			return null;
-		}else{
-			JSONObject json=new JSONObject();
-			json.put("userId", userId);
-			json.put("userName", userName);
-			json.put("keyWords", keyWords);
-			return json;
+		JSONObject json=new JSONObject();
+		json.put("keyWords", keyWords);
+		return json;
+	}
+	/**
+	 * kds的格式：[u'123',u'ces']
+	 * @param kds
+	 * @return
+	 */
+	private static String[] toArray(String kds) {
+		kds=unicode2String(kds);
+		System.out.println("【keyWords】"+kds);
+		String pattern="'([^ ']+)'";
+		Pattern p=Pattern.compile(pattern);
+		Matcher m=p.matcher(kds);
+		List<String> keyWords=new ArrayList<String>();
+		while(m.find()){
+			keyWords.add(m.group(1));
 		}
+		return keyWords.toArray(new String[]{});
+	}
+	/**
+	 * 1、把unicode的编码字符串转换为该编码对应的字符串
+	 * @param unicodeStr
+	 * @return
+	 */
+	public static String unicode2String(String unicodeStr){
+		Pattern p=Pattern.compile("(\\\\u(\\p{XDigit}{4}))");
+		char ch;
+		Matcher m=p.matcher(unicodeStr);
+		StringBuffer sb=new StringBuffer();
+		while(m.find()){
+			if(m.groupCount()>1){
+				ch=(char)Integer.parseInt(m.group(2),16);
+			}else{
+				ch=(char)Integer.parseInt(m.group(1),10);
+			}
+			m.appendReplacement(sb, ch+"");
+		}
+		m.appendTail(sb);
+		return sb.toString();
 	}
 	
-	private void sysPring(HttpServletRequest request){
+	private void sysPrint(HttpServletRequest request){
 		Enumeration<String> enums=request.getParameterNames();
 		while(enums.hasMoreElements()){
 			String key=enums.nextElement();
-			System.out.println("【公文搜索参数】"+key+"->"+toStr(request.getParameterValues(key)));
+			System.out.println("【搜索接口参数】"+key+"->"+toStr(request.getParameterValues(key)));
 		}
 	}
 	
