52c07c99df3fa4e0a1aa9c964064e898a3cddc17
zhouhaojie <15652791100@163.com>
2020-07-08 14:26:27 +0800

    1、修改业务配置默认页面为部管理员页面；2、部管理员配置列表新增单位名称显示列

--- src/main/resources/static/ywpz/main/js/index.js
+++ src/main/resources/static/ywpz/main/js/index.js
@@ -9,12 +9,17 @@ var pageModule = function(){
 				if(data=="3"){//即是部管理员又是局管理员,按照部管理员权限分配
 					$('#departAdmin').show();
 					$('#juAdmin').show();
+					$('#iframe100').attr("src","../../bglysz/html/index.html");
 				}else if (data=="0"||data=="1"){//超级管理员或部管理员
 					$('#departAdmin').show();
 					$('#juAdmin').show();
+                    $('#iframe100').attr("src","../../bglysz/html/index.html");
 				}else{ //局管理员
 					$('#juAdmin').show();
 					$('#departAdmin').hide();
+                    $('#iframe100').attr("src","../../glysz/html/index.html");
+                    $("#departAdmin").removeClass("active");
+                    $('#juAdmin').addClass("active");
 				}
 			}
 		});
